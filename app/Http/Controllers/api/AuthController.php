90cfea58ef97bf5f8c70d8114bc393b4538fed2d
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-08 13:07:10 +0200

    adding user data to login response

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\District;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -28,7 +29,8 @@ class AuthController extends Controller {
 		}
 		return $this->success(
 			[ 
-				'token' => $user->createToken( $user->email )->plainTextToken
+				'user' => collect( $user )
+					->merge( [ 'token' => $user->createToken( $user->email )->plainTextToken ] ),
 			],
 			message: 'Login Completed Successfully' );
 	}
@@ -39,20 +41,37 @@ class AuthController extends Controller {
 				'name' => 'required',
 				'email' => 'required|email|unique:users,email',
 				'password' => 'required',
+				'state_id' => 'required|exists:states,id',
+				'district_id' => 'required|exists:districts,id',
 			] );
 
 		if ( $validator->fails() ) {
 			return $this->handleValidation( $validator );
 		}
-
+		$validateRegister = $this->validateRegister( $validator );
+		if ( $validateRegister['fails'] ) {
+			return $validateRegister['response'];
+		}
 		$user = User::create( $validator->validated() );
-
 
 		return $this->success(
 			[ 
-				'token' => $user->createToken( $user->email )->plainTextToken,
-				'user' => $user,
+				'user' => collect( $user )
+					->merge( [ 'token' => $user->createToken( $user->email )->plainTextToken ] ),
 			],
 			message: 'Register Completed Successfully' );
+	}
+
+	protected function validateRegister( $validator ) {
+		$validated = $validator->validated();
+		$district = District::find( $validated['district_id'] );
+		if ( $district->state_id != $validated['state_id'] ) {
+			return [ 
+				'fails' => true,
+				'response' => $this->failure( "The District with id {$validated['district_id']} don't belong to this state with id {$validated['state_id']}" )
+			];
+		} else {
+			return [ 'fails' => false ];
+		}
 	}
 }
